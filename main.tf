1625fc338567114797919f5c51c6aaadee799b8c
Sonny Enchill <sonnyenchill@gmail.com>
2026-04-12 16:45:09 +0100

    Adjust MySQL flexible server zone 03

--- main.tf
+++ main.tf
@@ -201,7 +201,7 @@ resource "azurerm_mysql_flexible_server" "mysql" {
   administrator_password = var.mysql_admin_password
   sku_name               = "B_Standard_B1ms"
   version                = "8.0.21"
-  zone                   = "2"
+  zone                   = "3"
 }
 
 resource "azurerm_mysql_flexible_database" "db" {
